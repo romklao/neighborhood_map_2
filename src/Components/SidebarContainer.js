14117a998a4f9d7404f2615c08729d12ef6ab389
Romklao <rk120879@gmail.com>
2018-06-07 11:18:46 -0700

    feat: Create the function to allow users to click a location to show infowindow from the sidebar

--- src/Components/SidebarContainer.js
+++ src/Components/SidebarContainer.js
@@ -4,8 +4,20 @@ import escapeRegExp from 'escape-string-regexp';
 
 class SidebarContainer extends Component {
 
-  // static propTypes = {
-  // }
+  static propTypes = {
+  }
+
+  onOpenMarker = (e) => {
+    const { markers } = this.props;
+    const value = e.target.value;
+
+    for (let marker of markers) {
+      if (value === marker.title) {
+        window.google.maps.event.trigger(marker, 'click');
+        break;
+      }
+    }
+  }
 
   render() {
     const { markers } = this.props;
@@ -32,6 +44,7 @@ class SidebarContainer extends Component {
           <input
             type="submit"
             value="Filter"
+            onClick={this.searchPlaces}
           />
       </div>
     );
