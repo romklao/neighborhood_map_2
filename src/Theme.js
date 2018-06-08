296c58d1f6329847d081113419ac63bf53a6bc53
Romklao <rk120879@gmail.com>
2018-06-08 16:47:06 -0700

    feat: Create the file to store the arrays of differnce map styles

---
+++ src/Theme.js
@@ -0,0 +1,353 @@
+let theme = [
+  {
+    "featureType": "landscape",
+    "stylers": [
+      {
+          "hue": "#FFBB00"
+      },
+      {
+          "saturation": 43.400000000000006
+      },
+      {
+          "lightness": 37.599999999999994
+      },
+      {
+          "gamma": 1
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway",
+    "stylers": [
+      {
+          "hue": "#FFC200"
+      },
+      {
+          "saturation": -61.8
+      },
+      {
+          "lightness": 45.599999999999994
+      },
+      {
+          "gamma": 1
+      }
+    ]
+  },
+  {
+    "featureType": "road.arterial",
+    "stylers": [
+      {
+          "hue": "#FF0300"
+      },
+      {
+          "saturation": -100
+      },
+      {
+          "lightness": 51.19999999999999
+      },
+      {
+          "gamma": 1
+      }
+    ]
+  },
+  {
+    "featureType": "road.local",
+    "stylers": [
+      {
+          "hue": "#FF0300"
+      },
+      {
+          "saturation": -100
+      },
+      {
+          "lightness": 52
+      },
+      {
+          "gamma": 1
+      }
+    ]
+  },
+  {
+    "featureType": "water",
+    "stylers": [
+      {
+          "color": "#26A1D6"
+      },
+      {
+          "saturation": -13.200000000000003
+      },
+      {
+          "lightness": 2.4000000000000057
+      },
+      {
+          "gamma": 1
+      }
+    ]
+  },
+  {
+    "featureType": "poi",
+    "stylers": [
+      {
+          "hue": "#00FF6A"
+      },
+      {
+          "saturation": -1.0989010989011234
+      },
+      {
+          "lightness": 11.200000000000017
+      },
+      {
+          "gamma": 1
+      }
+    ]
+  }
+]
+
+let theme2 = [
+  {
+    "featureType": "all",
+    "elementType": "geometry.fill",
+    "stylers": [
+        {
+            "weight": "2.00"
+        }
+    ]
+  },
+  {
+    "featureType": "all",
+    "elementType": "geometry.stroke",
+    "stylers": [
+        {
+            "color": "#9c9c9c"
+        }
+    ]
+  },
+  {
+    "featureType": "all",
+    "elementType": "labels.text",
+    "stylers": [
+        {
+            "visibility": "on"
+        }
+    ]
+  },
+  {
+    "featureType": "landscape",
+    "elementType": "all",
+    "stylers": [
+        {
+            "color": "#f2f2f2"
+        }
+    ]
+  },
+  {
+    "featureType": "landscape",
+    "elementType": "geometry.fill",
+    "stylers": [
+        {
+            "color": "#ffffff"
+        }
+    ]
+  },
+  {
+    "featureType": "landscape.man_made",
+    "elementType": "geometry.fill",
+    "stylers": [
+        {
+            "color": "#ffffff"
+        }
+    ]
+  },
+  {
+    "featureType": "poi",
+    "elementType": "all",
+    "stylers": [
+        {
+            "visibility": "off"
+        }
+    ]
+  },
+  {
+    "featureType": "road",
+    "elementType": "all",
+    "stylers": [
+        {
+            "saturation": -100
+        },
+        {
+            "lightness": 45
+        }
+    ]
+  },
+  {
+    "featureType": "road",
+    "elementType": "geometry.fill",
+    "stylers": [
+        {
+            "color": "#eeeeee"
+        }
+    ]
+  },
+  {
+    "featureType": "road",
+    "elementType": "labels.text.fill",
+    "stylers": [
+        {
+            "color": "#7b7b7b"
+        }
+    ]
+  },
+  {
+    "featureType": "road",
+    "elementType": "labels.text.stroke",
+    "stylers": [
+        {
+            "color": "#ffffff"
+        }
+    ]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "all",
+    "stylers": [
+        {
+            "visibility": "simplified"
+        }
+    ]
+  },
+  {
+    "featureType": "road.arterial",
+    "elementType": "labels.icon",
+    "stylers": [
+        {
+            "visibility": "off"
+        }
+    ]
+  },
+  {
+    "featureType": "transit",
+    "elementType": "all",
+    "stylers": [
+        {
+            "visibility": "off"
+        }
+    ]
+  },
+  {
+    "featureType": "water",
+    "elementType": "all",
+    "stylers": [
+        {
+            "color": "#46bcec"
+        },
+        {
+            "visibility": "on"
+        }
+    ]
+  },
+  {
+    "featureType": "water",
+    "elementType": "geometry.fill",
+    "stylers": [
+        {
+            "color": "#c8d7d4"
+        }
+    ]
+  },
+  {
+    "featureType": "water",
+    "elementType": "labels.text.fill",
+    "stylers": [
+        {
+            "color": "#070707"
+        }
+    ]
+  },
+  {
+    "featureType": "water",
+    "elementType": "labels.text.stroke",
+    "stylers": [
+        {
+            "color": "#ffffff"
+        }
+    ]
+  }
+]
+
+let theme3 =
+[
+  {
+    "featureType": "landscape.natural",
+    "elementType": "geometry.fill",
+    "stylers": [
+        {
+            "visibility": "on"
+        },
+        {
+            "color": "#e0efef"
+        }
+    ]
+  },
+  {
+    "featureType": "poi",
+    "elementType": "geometry.fill",
+    "stylers": [
+        {
+            "visibility": "on"
+        },
+        {
+            "hue": "#1900ff"
+        },
+        {
+            "color": "#c0e8e8"
+        }
+    ]
+  },
+  {
+    "featureType": "road",
+    "elementType": "geometry",
+    "stylers": [
+        {
+            "lightness": 100
+        },
+        {
+            "visibility": "simplified"
+        }
+    ]
+  },
+  {
+    "featureType": "road",
+    "elementType": "labels",
+    "stylers": [
+        {
+            "visibility": "off"
+        }
+    ]
+  },
+  {
+    "featureType": "transit.line",
+    "elementType": "geometry",
+    "stylers": [
+        {
+            "visibility": "on"
+        },
+        {
+            "lightness": 700
+        }
+    ]
+  },
+  {
+    "featureType": "water",
+    "elementType": "all",
+    "stylers": [
+        {
+            "color": "#7dcdcd"
+        }
+    ]
+  }
+]
+
+export {theme, theme2, theme3};
+
+
+
