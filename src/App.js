4f3711065819771c28ad77afb8cfa6508e31d048
Romklao <rk120879@gmail.com>
2018-06-07 19:24:24 -0700

    feat: Adjust to center the map

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ class App extends Component {
     let self = this;
 
     self.map = new window.google.maps.Map(document.getElementById('map-container'), {
-      center: {lat:37.3852183, lng: -122.181167},
+      center: {lat:37.4192183, lng: -122.181167},
       zoom: 10,
       mapTypeControl: false
     });
