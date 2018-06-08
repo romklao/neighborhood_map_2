3e949a94cec1cb809d53f5479472de14a73cf286
Romklao <rk120879@gmail.com>
2018-06-07 18:58:11 -0700

    style: Move locations data to locations.json to be organized

--- src/App.js
+++ src/App.js
@@ -8,138 +8,7 @@ import './App.css';
 class App extends Component {
   state = {
     markers: [],
-    locations: [
-      {
-        title: 'Intel Museum',
-        location: {
-          lat: 37.387878,
-          lng: -121.963544
-        }
-      },
-      {
-        title: 'South Bay Historical Railroad Society',
-        location: {
-          lat: 37.353056,
-          lng: -121.936389
-        }
-      },
-      {
-        title: 'Palo Alto Baylands Nature Preserve',
-        location: {
-          lat: 37.459608, lng: -122.106412
-        }
-      },
-      {
-        title: 'Buck’s of Woodside',
-        location: {
-          lat: 37.429667, lng: -122.255111
-        }
-      },
-      {
-        title: 'Apple Campus',
-        location: {
-          lat: 37.33182,
-          lng: -122.03118
-        }
-      },
-      {
-        title: 'Ardenwood Historic Farm',
-        location: {
-          lat: 37.558056,
-          lng: -122.049444
-        }
-      },
-      {
-        title: 'Castle Rock State Park',
-        location: {
-          lat: 37.2306,
-          lng: -122.09568
-        }
-      },
-      {
-        title: 'Filoli Center',
-        location: {
-          lat: 37.4704,
-          lng: -122.310703
-        }
-      },
-      {
-        title: 'The Last Spike',
-        location: {
-          lat: 50.975278,
-          lng: -118.723611
-        }
-      },
-      {
-        title: 'Mt. Umunhum',
-        location: {
-          lat: 37.160502,
-          lng: -121.898567
-        }
-      },
-      {
-        title: 'The Winchester Mystery House',
-        location: {
-          lat: 37.318361,
-          lng: -121.950761
-        }
-      },
-      {
-        title: 'Peralta Adobe',
-        location: {
-          lat: 37.336403,
-          lng: -121.894753
-        }
-      },
-      {
-        title: 'Kelley Park',
-        location: {
-          lat: 37.323889,
-          lng: -121.861944
-        }
-      },
-      {
-        title: 'Stanford Memorial Church',
-        location: {
-          lat: 37.4268,
-          lng: -122.1705
-        }
-      },
-      {
-        title: 'San Jose Municipal Rose Garden',
-        location: {
-          lat: 37.33176,
-          lng: -121.92859
-        }
-      },
-      {
-        title: 'The Tech Museum of Innovation',
-        location: {
-          lat: 37.331437,
-          lng: -121.890728
-        }
-      },
-      {
-        title: 'San Mateo County History Museum',
-        location: {
-          lat: 37.486882,
-          lng: -122.229688
-        }
-      },
-      {
-        title: 'IBM’s New Almaden Research Lab',
-        location: {
-          lat: 37.211, lng: -121.805
-        }
-      },
-      {
-        title: 'New Almaden Quicksilver Mine Museum',
-        location: {
-          lat: 37.18,
-          lng: -121.835556
-        }
-      }
-    ]
+    locations: require('./Components/locations.json')
   }
 
   map = null;
@@ -160,7 +29,7 @@ class App extends Component {
       mapTypeControl: false
     });
 
-    this.createMarkers();
+    self.createMarkers();
   }
 
   createMarkers = () => {
@@ -202,8 +71,10 @@ class App extends Component {
           }
         });
       });
+
       self.markers.push(marker);
       self.setState({markers: self.markers});
+
     }
   }
 
@@ -219,7 +90,6 @@ class App extends Component {
     let radius = 100;
 
     let getStreetView = (data, status, ) => {
-      console.log('dataP, status', data, status)
       if (status === window.google.maps.StreetViewStatus.OK) {
 
         let nearStreetViewLocation = data.location.latLng;
@@ -244,8 +114,10 @@ class App extends Component {
             pitch: 5,
           }
         };
+
         let panoContainer = window.document.getElementById('pano');
-        let panorama = new window.google.maps.StreetViewPanorama(panoContainer, panoramaOptions);
+        new window.google.maps.StreetViewPanorama(panoContainer, panoramaOptions);
+
       } else {
 
         self.getYelpReviews(marker, 'rating-no-streetview');
@@ -260,9 +132,12 @@ class App extends Component {
         );
       }
     }
+
     streetViewService.getPanoramaByLocation(marker.position, radius, getStreetView);
     infowindow.open(self.map, marker);
-    infowindow.addListener('closeclick', () => {infowindow = null});
+    infowindow.addListener('closeclick', () => {
+      infowindow = null;
+    });
   }
 
   getPlacesDetails = (marker, elementId) => {
@@ -294,8 +169,10 @@ class App extends Component {
                    <p>${place.opening_hours.weekday_text[5]}</p>
                    <p>${place.opening_hours.weekday_text[6]}</p>`;
         }
+
         info += '<div>';
         document.getElementById(elementId).innerHTML = info;
+
       }
     });
   }
