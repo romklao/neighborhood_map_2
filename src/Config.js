7ce5517ececfa6e6d27fff486494eba9dce4ab26
Romklao <rk120879@gmail.com>
2018-08-26 19:10:15 -0700

    feat: Add the Config file to store the Yelp API key

---
+++ src/Config.js
@@ -0,0 +1,5 @@
+const config = {
+  headers: {'Authorization': 'Bearer EJPaqY46g2LsIT_W_fLoOVpW24sQ8SBLLJ4ddjEeCsUW6KIuF-W0twV2KcyjfEKGQ3prIyM6usqT4ne5M8XusGO4QB5WLKmUlMDY8F0Pw1w408_igZlCj6nbLJ4VW3Yx'}
+};
+
+export default config;
